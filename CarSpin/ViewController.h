8a66482bfe06c83389c827d06bdf23f8e7fa0474
Mo DeJong <mo@modejong.com>
2015-11-04 14:20:14 -0800

    Initial Commit

---
+++ CarSpin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarSpin
+//
+//  Created by Mo DeJong on 11/4/15.
+//  Copyright © 2015 helpurock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CarSpin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarSpin
+//
+//  Created by Mo DeJong on 11/4/15.
+//  Copyright © 2015 helpurock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
